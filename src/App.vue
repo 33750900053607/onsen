1b3eb2ce40d805969e10066b21ae8518b5b0b048
ashanti <as609@mtec.cf>
2019-03-09 11:39:29 +0900

    1st commit

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,60 @@
 <template>
   <div id="app">
+    <v-ons-page>
+      <v-ons-toolbar>
+        <div class="center">Title</div>
+      </v-ons-toolbar>
+
+      <p style="text-align: center">
+        <v-ons-button @click="$ons.notification.alert('Hello World!')">
+          Click me!
+        </v-ons-button>
+      </p>
+    </v-ons-page>
+    <v-ons-page>
+    <v-ons-toolbar class="home-toolbar">
+      <div class="left">
+        <v-ons-toolbar-button @click="$store.commit('splitter/toggle')">
+          <v-ons-icon icon="ion-navicon, material:md-menu"></v-ons-icon>
+        </v-ons-toolbar-button>
+      </div>
+      <div class="center">{{ msg }}</div>
+    </v-ons-toolbar>
+
+    <div class="header">
+      <img src="./assets/logo.png">
+    </div>
+
+    <v-ons-list-title>Vue.js Essential Links</v-ons-list-title>
+    <v-ons-list>
+      <v-ons-list-item v-for="item in essentialLinks" @click="goTo(item.link)" :key="item.link">
+        <div class="left"><v-ons-icon fixed-width :icon="item.icon"></v-ons-icon></div>
+        <div class="center">{{ item.label }}</div>
+        <div class="right"><v-ons-icon icon="fa-external-link"></v-ons-icon></div>
+      </v-ons-list-item>
+    </v-ons-list>
+
+    <v-ons-list-title>Vue.js Ecosystem</v-ons-list-title>
+    <v-ons-row>
+      <v-ons-col>
+        <v-ons-card @click="goTo('http://router.vuejs.org/')">vue-router</v-ons-card>
+      </v-ons-col>
+      <v-ons-col>
+        <v-ons-card @click="goTo('http://vuex.vuejs.org/')">vuex</v-ons-card>
+      </v-ons-col>
+    </v-ons-row>
+    <v-ons-row>
+      <v-ons-col>
+        <v-ons-card @click="goTo('http://vue-loader.vuejs.org/')">vue-loader</v-ons-card>
+      </v-ons-col>
+      <v-ons-col>
+        <v-ons-card @click="goTo('https://github.com/vuejs/awesome-vue')">awesome-vue</v-ons-card>
+      </v-ons-col>
+    </v-ons-row>
+
+  </v-ons-page>
+
+
     <div id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
@@ -7,6 +62,49 @@
     <router-view/>
   </div>
 </template>
+
+<script>
+export default {
+  name: 'home',
+  data () {
+    return {
+      msg: 'Welcome',
+      essentialLinks: [
+        {
+          label: 'Core Docs',
+          link: 'https://vuejs.org',
+          icon: 'fa-book'
+        },
+        {
+          label: 'Community Chat',
+          link: 'https://chat.vuejs.org',
+          icon: 'fa-commenting'
+        },
+        {
+          label: 'Forum',
+          link: 'https://forum.vuejs.org',
+          icon: 'ion-chatboxes'
+        },
+        {
+          label: 'Twitter',
+          link: 'https://twitter.com/vuejs',
+          icon: 'fa-twitter'
+        },
+        {
+          label: 'Docs for this template',
+          link: 'http://vuejs-templates.github.io/webpack/',
+          icon: 'fa-file-text'
+        }
+      ]
+    }
+  },
+  methods: {
+    goTo (url) {
+      window.open(url, '_blank')
+    }
+  }
+}
+</script>
 
 <style lang="scss">
 #app {
--- src/main.js
+++ src/main.js
@@ -1,8 +1,14 @@
+import 'onsenui/css/onsenui.css';
+import 'onsenui/css/onsen-css-components.css';
+
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
 import './registerServiceWorker'
+import VueOnsen from 'vue-onsenui'; // This already imports 'onsenui'
+
+Vue.use(VueOnsen);
 
 Vue.config.productionTip = false
 
